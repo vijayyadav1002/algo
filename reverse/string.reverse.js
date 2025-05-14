8f0e3bbdd9dfceea3fcde2ccaaa545820f645c81
Vijay Yadav <vijayyadav1002@gmail.com>
2025-05-14 14:11:23 -0500

    Add .idea to .gitignore and enhance string reversal methods

--- reverse/string.reverse.js
+++ reverse/string.reverse.js
@@ -7,6 +7,21 @@ String.prototype.reverse = function () {
     return result.join('');
 }
 
+
+String.prototype.reverse2 = function () {
+    const inArray = this.split('');
+    for (let start = 0, end = inArray.length - 1; start < end;) {
+        let tmp = inArray[start];
+        inArray[start] = inArray[end];
+        inArray[end] = tmp;
+        start++;
+        end--;
+    }
+    return inArray.join('');
+}
+
+
 const str = new String('abcd');
 
 console.log(str.reverse());
+console.log(str.reverse2());
