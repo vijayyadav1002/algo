2f895557c7e6a03e0a14597c534590c12eb8a8b3
Vijay Yadav <vijayyadav1002@gmail.com>
2025-05-14 16:32:34 -0500

    feat: Add nested.directory structure and toggleFolder functionality with tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
     testEnvironment: 'node',
     transform: {
         '^.+\\.ts$': 'ts-jest',
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
     }
 }
---
+++ src/companies/nested.directory.ts
@@ -0,0 +1,38 @@
+type DirectoryFile = {
+    name: string;
+    isOpen?: boolean;
+    files?: DirectoryFile[];
+}
+
+
+export const sampleFiles: DirectoryFile[] = [
+    {
+        name: "src",
+        isOpen: true,
+        files: [
+            {
+                name: "companies",
+                isOpen: false,
+                files: [
+                    {
+                        name: "nested.directory.ts"
+                    },
+                    {
+                        name: "nested.directory.test.ts"
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+export const toggleFolder = (parts: string[], files: DirectoryFile[]) => {
+    const [currentName, ...rest] = parts;
+    const file = files.find((value: DirectoryFile) => value.name == currentName)
+    if (rest.length > 0 && file?.files) {
+        file.files = toggleFolder(rest, file.files);
+    } else if (file && file.isOpen !== undefined) {
+        file.isOpen = !file.isOpen;
+    }
+    return files
+}
