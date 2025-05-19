ba7bca889d49055065ac68f2a7b2f5b2b4882323
Vijay Yadav <vijayyadav1002@gmail.com>
2025-05-19 16:58:48 -0500

    Update is_unique.ts

--- src/leetcode/c1_arrays_and_strings/is_unique.ts
+++ src/leetcode/c1_arrays_and_strings/is_unique.ts
@@ -1,3 +1,6 @@
+// To find if all the elements in string are unique
+
+
 const is_unique = (input: string): boolean => {
   const char_map: { [key: string]: boolean } = {};
   for (let i = 0; i < input.length; i++) {
