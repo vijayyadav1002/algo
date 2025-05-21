6ade2f53a6316dc6864fa81c8ebe344e0a278024
Vijay Yadav <vijayyadav1002@gmail.com>
2025-05-21 10:03:32 -0500

    feat: Enhance isAnagram and isAnagramOptimized functions with additional tests and improved structure

--- src/leetcode/c1_arrays_and_strings/valid_anagram.test.ts
+++ src/leetcode/c1_arrays_and_strings/valid_anagram.test.ts
@@ -1,13 +1,24 @@
-import { isAnagram } from './valid_anagram';
+import { isAnagram, isAnagramOptimized } from './valid_anagram';
 
-describe('isAnagram', () => {
+describe('Anagram Tests', () => {
   const testSet = [
     ['anagram', 'nagaram', true],
     ['rat', 'car', false],
+    ['carr', 'car', false],
   ];
-  testSet.forEach((data: any[]) => {
-    test(`test if ${data[0]} is anagram of ${data[1]}`, () => {
-      expect(isAnagram(data[0], data[1])).toBe(data[2]);
+  describe('isAnagram', () => {
+    testSet.forEach((data: any[]) => {
+      test(`test if ${data[0]} is anagram of ${data[1]}`, () => {
+        expect(isAnagram(data[0], data[1])).toBe(data[2]);
+      });
+    });
+  });
+
+  describe('isAnagramOptimized', () => {
+    testSet.forEach((data: any[]) => {
+      test(`test if ${data[0]} is anagram of ${data[1]}`, () => {
+        expect(isAnagramOptimized(data[0], data[1])).toBe(data[2]);
+      });
     });
   });
 });
--- src/leetcode/c1_arrays_and_strings/valid_anagram.ts
+++ src/leetcode/c1_arrays_and_strings/valid_anagram.ts
@@ -1,7 +1,19 @@
-// https://leetcode.com/problems/valid-anagram/
+/**
+ * https://leetcode.com/problems/valid-anagram/
+ * constraints:
+ * 1 <= s.length, t.length <= 5 * 104
+ * s and t consist of lowercase English letters.
+ */
 
+/**
+ *
+ * @param source
+ * @param target
+ * @returns
+ */
 const isAnagram = (source: string, target: string): boolean => {
   const char_map: { [key: string]: number } = {};
+  if (source.length !== target.length) return false;
   for (let i = 0; i < source.length; i++) {
     const char = source.charAt(i);
     char_map[char] = (char_map?.[char] || 0) + 1;
@@ -17,4 +29,20 @@ const isAnagram = (source: string, target: string): boolean => {
   return true;
 };
 
-export { isAnagram };
+// Complexity is O(n) since 26 is constant
+const isAnagramOptimized = (source: string, target: string): boolean => {
+  let chars = new Array(26).fill(0),
+    matches = 0;
+  const offset = 'a'.charCodeAt(0);
+  if (source.length !== target.length) return false;
+  for (let i = 0; i < source.length; i++) {
+    chars[source[i].charCodeAt(0) - offset] += 1;
+    chars[target[i].charCodeAt(0) - offset] -= 1;
+  }
+  for (let i = 0; i < chars.length; i++) {
+    if (chars[i] === 0) matches++;
+  }
+  return matches === 26;
+};
+
+export { isAnagram, isAnagramOptimized };
